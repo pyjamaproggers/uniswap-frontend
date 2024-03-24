cb8d2b8162b979f11fb3dff69e514d0a8b4eba78
iamaryanyadavv <aryanyadav1601@gmail.com>
2024-03-25 04:14:24 +0530

    added google sign in

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useState, useEffect} from "react";
+import jwt_decode from "jwt-decode";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function handleCallbackresponse(response) {
+        var userObject = jwt_decode(response.credential)
+        document.getElementById("GoogleButton").hidden = true;
+        console.log(userObject)
+    }
+
+    useEffect(() => {
+        window.setTimeout(()=>{
+            window.google.accounts.id.initialize({
+                client_id: process.env.REACT_APP_GOOGLE_CLIENTID,
+                callback: handleCallbackresponse
+            });
+            
+            window.google.accounts.id.renderButton(
+                document.getElementById("GoogleButton"),
+                { theme: 'outlined', size: 'large', shape: 'pill',}
+            ); 
+        }, 2000)
+
+    }, [])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <div className="GoogleButton" id='GoogleButton'></div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENTID}>
+        <App />
+    </GoogleOAuthProvider>
 );
+
+// Pyjama Programmers CLientID: 84838878723-s4pc8fhltthe59h5epk6v49lldooeukq.apps.googleusercontent.com
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
