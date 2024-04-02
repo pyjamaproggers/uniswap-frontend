0beaf273d22b83094711ff1dee0aac7fdfcff530
iamaryanyadavv <aryanyadav1601@gmail.com>
2024-04-02 10:58:38 +0530

    added logout feature on unauthorized

--- src/pages/ErrorAuthPage/ErrorAuthPage.js
+++ src/pages/ErrorAuthPage/ErrorAuthPage.js
@@ -3,6 +3,44 @@ import { Button, Col, Grid, Text } from "@nextui-org/react";
 import './ErrorAuthPage.css'
 
 export default function ErrorAuthPage(){
+
+    const backend = process.env.REACT_APP_BACKEND
+
+    function handleLogout() {
+        fetch(`${backend}/api/auth/logout`, {
+            method: 'POST',
+            credentials: 'include', // Necessary to include the cookie in requests
+        })
+            .then(res => {
+                if (res.ok) {
+                    // Assuming the backend has now invalidated the session/cookie...
+
+                    // Clear client-side storage of user details
+                    localStorage.removeItem('userEmail');
+                    localStorage.removeItem('userName');
+                    localStorage.removeItem('userPicture');
+                    localStorage.removeItem('itemsPosted');
+                    localStorage.removeItem('favouriteItems');
+                    localStorage.removeItem('contactNumber');
+                    localStorage.clear()
+                    // Redirect user to the homepage or login page
+                    window.location.pathname = '/' // Adjust the path as necessary for your application
+                } else {
+                    throw new Error('Logout failed');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Optionally handle the error, maybe show a notification to the user
+            });
+    }
+
+    useEffect(()=>{
+        window.setTimeout(()=>{
+            handleLogout()
+        }, 2000)
+    })
+
     return(
         <div className="errorAuth-bg">
             <Col
