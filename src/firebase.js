87a2690083d35eafd1182b7dadf2a75ea2321ab3
zahaanshapoorjee <zahaan@shapoorjee.com>
2024-04-04 18:26:53 +0530

    new firebase reset

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken } from "firebase/messaging";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getMessaging, getToken } from "firebase/messaging";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAawNBBBRIkCbRzbs4Sxu5-O_M_shnW0Ok",
-    authDomain: "uniswap-418221.firebaseapp.com",
-    projectId: "uniswap-418221",
-    storageBucket: "uniswap-418221.appspot.com",
-    messagingSenderId: "84838878723",
-    appId: "1:84838878723:web:ea20d2112707c489384836",
-    measurementId: "G-FYFKW26X8K"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAawNBBBRIkCbRzbs4Sxu5-O_M_shnW0Ok",
+//     authDomain: "uniswap-418221.firebaseapp.com",
+//     projectId: "uniswap-418221",
+//     storageBucket: "uniswap-418221.appspot.com",
+//     messagingSenderId: "84838878723",
+//     appId: "1:84838878723:web:ea20d2112707c489384836",
+//     measurementId: "G-FYFKW26X8K"
+//   };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-const messaging = getMessaging(app);
+// const messaging = getMessaging(app);
