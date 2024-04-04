12d8eb497a87c642be0f341577ddcab356bbaa0a
zahaanshapoorjee <zahaan@shapoorjee.com>
2024-04-04 19:14:03 +0530

    style cleanup

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('./firebase-messaging-sw.js').then(function (registration) {
-            console.log('Service Worker registered successfully with scope: ', registration.scope);
-        }, function (err) {
-            console.log('Service Worker registration failed: ', err);
-        });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function () {
+//         navigator.serviceWorker.register('./firebase-messaging-sw.js').then(function (registration) {
+//             console.log('Service Worker registered successfully with scope: ', registration.scope);
+//         }, function (err) {
+//             console.log('Service Worker registration failed: ', err);
+//         });
+//     });
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
