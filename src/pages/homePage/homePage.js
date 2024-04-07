49ca0d61eaf1d058dac5c014cb96d40960059461
iamaryanyadavv <aryanyadav1601@gmail.com>
2024-04-07 15:29:48 +0530

    styled homepage add to hs tutorial

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -37,9 +37,9 @@ export default function HomePage(props) {
 
     return (
         <>
-         {isMobile && showPwaBanner && (
+            {isMobile && showPwaBanner && (
                 <Grid.Container css={{
-                    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+                    backgroundColor: 'rgba(0, 0, 0, 0.9)',
                     padding: '10px',
                     position: 'fixed',
                     top: 0,
@@ -48,52 +48,76 @@ export default function HomePage(props) {
                 }}>
                     <Col>
                         <Text css={{ color: '$white' }}>
-                            You can now get UniSwap as an app. <span style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={scrollToPwaTutorial}>Find out how</span> or <span style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={() => setShowPwaBanner(false)}>hide this message</span>
+                            You can now get UniSwap as an app. <span style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={() => { scrollToPwaTutorial(); setShowPwaBanner(false) }}>Find out how</span> or <span style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={() => setShowPwaBanner(false)}>hide this message</span>
                         </Text>
                     </Col>
                 </Grid.Container>
             )}
-        <Grid.Container className="homebg" css={{
-            jc: 'center',
-            alignItems: '',
-            height: window.screen.height - 200,
-            paddingTop: '25%'
-            // padding: '10% 0px 40% 0'
-        }}>
-            <div className="content">
-                <Col css={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }} >
-                    <Text css={{
-                        '@xsMin': {
-                            fontSize: '$3xl',
-                            fontWeight: '$semibold'
-                        },
-                        '@xsMax': {
-                            fontSize: '$3xl',
-                            fontWeight: '$medium',
-                            padding: '0px 8px',
-                            lineHeight: '1.3'
-                        },
-                        textAlign: 'center',
-                        color: '$white'
-                    }}>
-                        Official UniSwap™ Website
-                    </Text>
-                    {isSignedIn ?
-                        <>
-                            <Col css={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center'
-                            }}>
+            <Grid.Container className="homebg" css={{
+                jc: 'center',
+                alignItems: '',
+                height: window.screen.height - 200,
+                paddingTop: '25%'
+                // padding: '10% 0px 40% 0'
+            }}>
+                <div className="content">
+                    <Col css={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }} >
+                        <Text css={{
+                            '@xsMin': {
+                                fontSize: '$3xl',
+                                fontWeight: '$semibold'
+                            },
+                            '@xsMax': {
+                                fontSize: '$3xl',
+                                fontWeight: '$medium',
+                                padding: '0px 8px',
+                                lineHeight: '1.3'
+                            },
+                            textAlign: 'center',
+                            color: '$white'
+                        }}>
+                            Official UniSwap™ Website
+                        </Text>
+                        {isSignedIn ?
+                            <>
+                                <Col css={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center'
+                                }}>
+                                    <Text css={{
+                                        '@xsMin': {
+                                            fontSize: '$xl',
+                                            fontWeight: '$medium',
+                                            padding: '0px 0px 8px 0px'
+                                        },
+                                        '@xsMax': {
+                                            fontSize: '$base',
+                                            fontWeight: '$medium',
+                                            padding: '8px',
+                                        },
+                                        textAlign: 'center',
+                                        color: '$white'
+                                    }}>
+                                        Looking for Apparel? Food? Tickets? Jewellry? Stationery? We have it all here.
+                                    </Text>
+                                    <Button auto flat color={'error'} onClick={() => {
+                                        window.location.pathname = '/saleitems'
+                                    }}>
+                                        Items On Sale →
+                                    </Button>
+                                </Col>
+                            </>
+                            :
+                            <>
                                 <Text css={{
                                     '@xsMin': {
                                         fontSize: '$xl',
-                                        fontWeight: '$medium',
-                                        padding: '0px 0px 8px 0px'
+                                        fontWeight: '$medium'
                                     },
                                     '@xsMax': {
                                         fontSize: '$base',
@@ -103,60 +127,36 @@ export default function HomePage(props) {
                                     textAlign: 'center',
                                     color: '$white'
                                 }}>
-                                    Looking for Apparel? Food? Tickets? Jewellry? Stationery? We have it all here.
+                                    Please sign in using your university email ID using the google button located at the top-right ↑
                                 </Text>
-                                <Button auto flat color={'error'} onClick={() => {
-                                    window.location.pathname = '/saleitems'
-                                }}>
-                                    Items On Sale →
-                                </Button>
-                            </Col>
-                        </>
-                        :
-                        <>
-                            <Text css={{
-                                '@xsMin': {
-                                    fontSize: '$xl',
-                                    fontWeight: '$medium'
-                                },
-                                '@xsMax': {
-                                    fontSize: '$base',
-                                    fontWeight: '$medium',
-                                    padding: '8px',
-                                },
-                                textAlign: 'center',
-                                color: '$white'
-                            }}>
-                                Please sign in using your university email ID using the google button located at the top-right ↑
-                            </Text>
-                            
-                        </>
-                    }
-                </Col>
-            </div>
-        </Grid.Container>
-        {isMobile && ( 
+
+                            </>
+                        }
+                    </Col>
+                </div>
+            </Grid.Container>
+            {isMobile && (
                 <div className="add-to-home-screen-tutorial" ref={pwaTutorialRef}>
-                    <Text css={{ textAlign: 'center', color: '$white', padding: '20px' }}>
-                        <strong>How to Add to Home Screen</strong>
+                    <Text css={{ textAlign: 'center', color: '$white', padding: '20px', fontWeight: '$semibold' }}>
+                        How do I "Add to Home Screen"?
                     </Text>
                     <div>
                         <img src={pwaStep1} alt="Step 1" style={{ width: '100%', padding: '10px' }} />
-                        <Text css={{ textAlign: 'center', color: '$white' }}>Tap the 'Share' icon at the bottom of the screen, or the top right if you're using an iPad:</Text>
+                        <Text css={{ textAlign: 'center', color: '$white', fontWeight: '$medium', padding: '0px 20px 20px 20px' }}>Tap the 'Share' icon at the bottom of the screen, or the top right if you're using an iPad:</Text>
                     </div>
                     <div>
                         <img src={pwaStep2} alt="Step 2" style={{ width: '100%', padding: '10px' }} />
-                        <Text css={{ textAlign: 'center', color: '$white' }}>Scroll down and select 'Add to Home Screen':</Text>
+                        <Text css={{ textAlign: 'center', color: '$white', fontWeight: '$medium', padding: '0px 20px 20px 20px' }}>Scroll down and select 'Add to Home Screen':</Text>
                     </div>
                     <div>
                         <img src={pwaStep3} alt="Step 3" style={{ width: '100%', padding: '10px' }} />
-                        <Text css={{ textAlign: 'center', color: '$white' }}>Tap 'Add' on the top right.</Text>
+                        <Text css={{ textAlign: 'center', color: '$white', fontWeight: '$medium', padding: '0px 20px 20px 20px' }}>Tap 'Add' on the top right.</Text>
                     </div>
                     <div>
-                        <Text css={{ textAlign: 'center', color: '$white' }}>Done! You can now enjoy UniSwap from your home screen.</Text>
+                        <Text css={{ textAlign: 'center', color: '$white', fontWeight: '$medium', padding: '0px 20px 20px 20px' }}>Done! You can now enjoy UniSwap from your home screen.</Text>
                     </div>
                 </div>
             )}
-    </>
+        </>
     )
 }
